d78b351d4262f82eb464ed3ded31ccbdd042e483
ybounite <dimayoussef42@gmail.com>
2025-11-16 09:45:38 +0100

    Add Request class boilerplate with constructors, copy operations, and destructor

---
+++ Request.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                    file: Request.cpp                                       */
+/*                    by: ybounite <ybounite@student.1337.ma>                 */
+/*                                                                            */
+/*                    Created: 2025/11/16 09:42 by ybounite                   */
+/*                    Updated: 2025/11/16 09:42 by ybounite                   */
+/* ************************************************************************** */
+
+# include "Request.hpp"
+
+
+Request::Request( void ) {
+
+}
+
+Request::Request( const  Request &Other ) {
+
+}
+
+Request &Request::operator=( const  Request &Other ) {
+
+}
+
+Request::~Request( void ) {
+
+}
