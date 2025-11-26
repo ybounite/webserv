1fd965c05941d469c8ae445616082dedb077948d
mmilyass <ilyassmeftah06@gmail.com>
2025-11-26 11:02:00 +0100

    feat: implement Socket class with client handling and HTTP response functionality

--- main.cpp
+++ main.cpp
@@ -5,11 +5,12 @@ int main()
     try
     {
         Socket sct;
+        sct.run();
+
     }
     catch (const std::exception &e)
     {
         std::cout << "Error: " << e.what() << std::endl;
         return 1;
     }
-
 }
