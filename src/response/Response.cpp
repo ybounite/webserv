126e5ee31dd41b101b8671b9d70c2bc48795476f
ybounite <dimayoussef42@gmail.com>
2025-12-01 16:21:17 +0100

    Implement response

---
+++ src/response/Response.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                    file: Response.cpp                                      */
+/*                    by: ybounite <ybounite@student.1337.ma>                 */
+/*                                                                            */
+/*                    Created: 2025/11/29 16:02 by ybounite                   */
+/*                    Updated: 2025/11/29 16:02 by ybounite                   */
+/* ************************************************************************** */
+
+# include "Response.hpp"
+
+Response::Response(void ) {
+
+}
+Response::Response( const  Response &Other ) {
+    (void)Other;
+}
+
+Response::~Response( void ) {
+    
+}
+/*
+Response    Response::handle(const Request &req, const ServerConfig &config) {
+    (void)req;
+    (void)config;
+    return Response();
+}
+*/
