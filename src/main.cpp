9d60fbbe4a33e3bbe51c42268da085f24fad4971
ybounite <dimayoussef42@gmail.com>
2025-12-01 16:25:24 +0100

    feat: add global Config GbConfig and initialize it in main

--- src/main.cpp
+++ src/main.cpp
@@ -8,17 +8,8 @@
 
 #include "../includes/Webserv.hpp"
 
-
-void printVector(const std::vector<std::string>& vec, const std::string& name)
-{
-    std::cout << name << ": ";
-    std::cout << std::endl;
-    for (size_t i = 0; i < vec.size(); i++)
-    {
-        std::cout << vec[i] ;
-        std::cout << std::endl;
-    }
-}
+// global class
+class   Config  GbConfig;
 
 int main(int argc, char const *argv[]) {
 
@@ -28,12 +19,11 @@ int main(int argc, char const *argv[]) {
 			std::string	config_path;
 			Tokenizer	token;
 			Parser	parser;
-			Config	config;
 			config_path = (argc == 1 ? "configs/default.conf" : argv[1]);
 			vector = token.tokenize(ConfigFileReader::read(config_path));
-			config = parser.parse(vector);
+			GbConfig = parser.parse(vector);
 			// printVector(vector, "vector");
-			Server sct(config);
+			Server sct(GbConfig);
 			sct.run();
 		} catch(const std::exception& e) {
 			std::cerr << e.what() << std::endl;
@@ -45,16 +35,3 @@ int main(int argc, char const *argv[]) {
 	}
 	return 0;
 }
-
-
-// int main()
-// {
-//     Msg::info("Reading configuration file...");
-//     Msg::success("Server started on port 8080");
-//     Msg::warning("Missing index directive");
-//     Msg::error("Failed to open file!");
-//     Msg::debug("Token count = 153");
-//     Msg::print("Hello sodahani!", LIGHTMAGENTA);
-//     Msg::print("Danger!", BLINK);
-//     Msg::print("Sky Blue!", LIGHT_BLUE);
-// }
