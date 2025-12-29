5222c470a13a0c4c14015520154652eef342ef35
ybounite <dimayoussef42@gmail.com>
2025-12-29 21:22:47 +0100

    Enhance RequestHandler by adding error response handling and refining utility functions

--- src/request/RequestHandler.hpp
+++ src/request/RequestHandler.hpp
@@ -18,9 +18,11 @@ class Response;
 class RequestHandler {
 public:
 	enum	enHttpMethod { HTTP_GET, HTTP_POST, HTTP_DELETE, HTTP_UNKNOWN };
+	static const ServerConfig &server;
 	// Static handler - processes Request and returns Response
 	static Response		handle( const Request &req, const Config &config );
 	static short		getMothod( const std::string &method );
+	static Response		BuildErrorResponse( short code );
 
 private:
 	// Helper methods for different HTTP methods
@@ -29,6 +31,14 @@ private:
 	static Response		handleDELETE(const Request &req, const ServerConfig &config);
 	
 	// Utility functions
-	static std::string	readFile(const std::string &path);
-	static std::string	getErrorPage(int statusCode);
+	static std::string			readFile(const std::string &path);
+	static std::string			getErrorPage(int statusCode);
+	//static std::string			_BuildFileSystemPath(std::string Root, std::string Uri);
+	static std::string			_BuildFileSystemPath(const std::string &root, const std::string &uri);
+
+	static bool					_ResourceExists( std::string &Path );
+	//static std::string			_ResolveIndexFile( std::string Path, const ServerConfig &server );
+	static std::string			_ResolveIndexFile(const std::string &path, const ServerConfig &server, const LocationConfig &loc);
+	static Response 			serveFile(const std::string &path);
+	static Response 		generateAutoindexResponse(const std::string &dirPath, const std::string &uri);
 };
